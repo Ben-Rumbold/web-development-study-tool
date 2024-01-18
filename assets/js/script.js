096dbc19bed60df22a5e1a2fa2dc2bfe4d1d7b96
6nhuseyin <6nhuseyin@gmail.com>
2024-01-18 21:49:03 +0000

    add modal logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+// modal function
+$(document).ready(function () {
+  $("#myModal").modal("show");
+
+  // Handle button clicks
+  $("#okayBtn, #closeBtn").on("click", function () {
+    $("#myModal").modal("hide");
+  });
+});
+
 const youtubeAPIkey = "AIzaSyASRVTIz4SSVmUIFEnAiuaJqLwH8XwuyVg"; //Youtube API
 const searchInputEl = $("#search-input"); // search input element
 const searchQuery = searchInputEl.val(); // search input value
